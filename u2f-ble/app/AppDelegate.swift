250c11f3ea91d8861f51b087d30d4cbf4b747b98
James Billingham <git@jamesbillingham.com>
2018-12-07 01:15:23 +0000

    url scheme basics

--- u2f-ble/app/AppDelegate.swift
+++ u2f-ble/app/AppDelegate.swift
@@ -16,4 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		// Override point for customization after application launch.
 		return true
 	}
+
+	func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+		return false
+	}
 }
